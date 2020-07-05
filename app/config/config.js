5eed36e5683e5171300b42cdd074061f5ecb9809
Pablo Garcia <pablodef_13@outlook.com>
2020-07-05 00:40:49 -0600

    app

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,6 @@
 module.exports={
     PORT:process.env.PORT || 22351,
-    DB:process.env.DB || 'mongodb+srv://user:user@cluster0.evu14.mongodb.net/LotoDB?retryWrites=true&w=majority'
+    DB:process.env.DB ||  'mongodb+srv://user:user@cluster0.evu14.mongodb.net/LotoDB?retryWrites=true&w=majority'//'mongodb+srv://user:user@cluster0.evu14.mongodb.net/LotoDB?retryWrites=true&w=majority'
     // 'mongodb+srv://pablo:H1IKgJ2J691ibX2z@cluster0.atqqj.mongodb.net/LotoDB?retryWrites=true&w=majority' 
     // 'mongodb://localhost:27017/LotoDB'
 }
